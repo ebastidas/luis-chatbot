291f815383c13f8945837aaa3b09ab7cb39c28f4
ebastidas <ebastidas3@gmail.com>
2019-12-08 19:13:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ibm-nestle
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ibm-nestle)
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+
+/**
+ * Declares the WebChat property on the window object.
+ */
+declare global {
+    interface Window {
+        WebChat: any;
+    }
+}
+
+window.WebChat = window.WebChat || {};
+
+@Component({
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"]
+})
+export class AppComponent implements OnInit {
+    @ViewChild("botWindow") botWindowElement: ElementRef;
+
+    public ngOnInit(): void {
+        const directLine = window.WebChat.createDirectLine({
+            secret: "PLKN8x6d_Yw.fMe325H_yjTqPCDzs23JE6jiKmu1jXuq3vN3Eurmdkk",
+            webSocket: false
+        });
+
+        window.WebChat.renderWebChat(
+            {
+                directLine: directLine,
+                userID: "USER_ID"
+            },
+            this.botWindowElement.nativeElement
+        );
+
+        directLine
+            .postActivity({
+                from: { id: "USER_ID", name: "USER_NAME" },
+                name: "requestWelcomeDialog",
+                type: "event",
+                value: "token"
+            })
+            .subscribe(
+                id => console.log(`Posted activity, assigned ID ${id}`),
+                error => console.log(`Error posting activity ${error}`)
+            );
+    }
+}
